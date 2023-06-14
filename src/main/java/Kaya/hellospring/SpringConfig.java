b504b200731390ef2341482e6214ac0c529b9e3a
Minju Kang <68880055+kayamz@users.noreply.github.com>
2023-06-15 02:53:24 +0900

    스프링 DB 접근 기술
    1. 순수 Jdbc
    쿼리 하나하나 작성. 복잡.
    
    2. 스프링 통합 테스트
    
    3. 스프링 JdbcTemplate
    반복되는 코드가 줄어들지만 SQL는 직접 작성
    
    4. JPA
    기본적인 CRUD 하는데에 쿼리 작성할 필요 없음
    단, select할 때는 JPQL 짜야함
    
    5. 스프링 데이터 JPA
    구현 클래스 작성할 필요 없음
    인터페이스만으로 끝나버림
    findBy ~ 제공해줌

--- src/main/java/Kaya/hellospring/SpringConfig.java
+++ src/main/java/Kaya/hellospring/SpringConfig.java
@@ -1,21 +1,36 @@
 package Kaya.hellospring;
 
-import Kaya.hellospring.repository.MemberRepository;
-import Kaya.hellospring.repository.MemoryMemberRepository;
+import Kaya.hellospring.repository.*;
 import Kaya.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
